bef24f04aefc89cd87fd7750ec02a84b033aed67
Nic Schworer <nic.schworer@gmail.com>
2020-09-15 10:09:38 -0400

    stuff

--- assets/script.js
+++ assets/script.js
@@ -12,10 +12,6 @@ var lon = '';
 var searchedArray = [];
 
 
-
-
-
-
 // API
     // https://openweathermap.org/forecast5
     // api key = 19ead611ac1f13b1c00469e35bb98b61
@@ -23,19 +19,6 @@ var searchedArray = [];
 
 currentDayEl.html(moment().format('LLLL'));
 loadSearched();
-
-
-// EVENT LISTENERS
-searchButton.on("click", function () {
-    runSearchFromSearch();
-    loadSearched();
-    // runSearch();
-    // getUVIndex();
-    // getForecast();
-});
-
-
-
 
 // FUNCTIONS
 
@@ -50,12 +33,11 @@ function loadSearched () {
         previousSearchesEl.append(psbutton);
         }
     }
-
 }
 
 
 
-function runSearchFromSearch () {
+function runSearchfromSearch () {
     currentConditionsEl.empty();
     forecastEl.empty();
     citySearch = cityInput.val();
@@ -106,9 +88,60 @@ function runSearchFromSearch () {
         localStorage.setItem("searched", JSON.stringify(searchedArray));
 };
             
+function runSearchfromPrevious (cityName) {
+    currentConditionsEl.empty();
+    forecastEl.empty();
+    coordQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=19ead611ac1f13b1c00469e35bb98b61";
+    $.ajax({
+        url: coordQueryURL,
+        method: "GET"
+        }).then(function(r) {
+        lat = r.city.coord.lat;
+        lon = r.city.coord.lon;
+        var cityTitle = $("<h2>").text(r.city.name + "   ");
+        var weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + r.list[0].weather[0].icon + "@2x.png");
+        var cityTemp = $("<p>").text("Temperature: " + ((r.list[0].main.temp - 273.15)*1.8+32).toFixed(0) + " degrees fahrenheit");
+        var cityHumidity = $("<p>").text("Humidity: " + r.list[0].main.humidity + "%");
+        var windSpeed = $("<p>").text("Wind Speed: " + r.list[0].wind.speed + " MPH");
+        currentConditionsEl.append(cityTitle.append(weatherIcon), cityTemp, cityHumidity, windSpeed);
         
+        fullQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=19ead611ac1f13b1c00469e35bb98b61";
+        $.ajax({
+            url: fullQueryURL,
+            method: "GET"
+        }).then(function(re) {
+            var uvIndex = $("<p>").text("UV Index: " + re.current.uvi).attr("id", "uv");
+            if (re.current.uvi < 3) {uvIndex.attr("id", "uv-low")};
+            if (re.current.uvi > 8) {uvIndex.attr("id", "uv-high")};
+            currentConditionsEl.append(uvIndex);
+            var fiveDayForecast = re.daily
+            console.log(fiveDayForecast);
+            for (var i = 1; i < 6; i++) {
+                var fcard = $("<div>").attr("class", "col-sm-2")
+                var fdate = $("<h5>").text(moment().add(i,'days').format("L"));
+                var ficon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + fiveDayForecast[i].weather[0].icon + "@2x.png");
+                var ftemp = $("<p>").text("Temp: " + ((fiveDayForecast[i].temp.day - 273.15)*1.8+32).toFixed(0) + "F");
+                var fhumidity = $("<p>").text("Humidity: " + fiveDayForecast[i].humidity + "%");
+
+                forecastEl.append(fcard.append(fdate, ficon, ftemp, fhumidity));
 
 
+            }
+            
+        });
+    });
+}        
+
+// EVENT LISTENERS
+searchButton.on("click", function () {
+    runSearchfromSearch();
+    loadSearched();
+});
+
+$(".psearch").on("click", function () {
+    runSearchfromPrevious($(this).text());
+    loadSearched();
+});
 
 
 
