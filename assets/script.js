6a41e5208582bd3b788aa3608e2cb47256351b2c
Nic Schworer <nic.schworer@gmail.com>
2020-09-15 20:31:29 -0400

    final touches

--- assets/script.js
+++ assets/script.js
@@ -23,13 +23,21 @@ loadSearched();
 // FUNCTIONS
 
 function loadSearched () {
+    // clear out previously added buttons
     previousSearchesEl.empty();
+    // get item from local storage
     searchedArray = JSON.parse(localStorage.getItem("searched"));
+    // if there is no item, stop
     if (searchedArray === null) {
         return
+    // loop through the array    
     } else {
         for (var i = 0; i < searchedArray.length; i++) {
-        var psbutton = $("<button>").text(searchedArray[i]).attr("class", "psearch")
+    // create a button for each value and append to the page        
+        var psbutton = $("<button>").text(searchedArray[i]).attr({
+            class: "psearch",
+        
+        })
         previousSearchesEl.append(psbutton);
         }
     }
@@ -38,16 +46,21 @@ function loadSearched () {
 
 
 function runSearchfromSearch () {
+    // clear previously searched weather data
     currentConditionsEl.empty();
     forecastEl.empty();
+    // get & store user city input
     citySearch = cityInput.val();
+    // create & call initial ajax URL
     coordQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=19ead611ac1f13b1c00469e35bb98b61";
     $.ajax({
         url: coordQueryURL,
         method: "GET"
       }).then(function(r) {
+        // get the lat & lon for future use
         lat = r.city.coord.lat;
         lon = r.city.coord.lon;
+        // get city name, weather icon, temp, humidity, & windspeed and add to page
         var cityTitle = $("<h2>").text(r.city.name + "   ");
         var weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + r.list[0].weather[0].icon + "@2x.png");
         var cityTemp = $("<p>").text("Temperature: " + ((r.list[0].main.temp - 273.15)*1.8+32).toFixed(0) + " degrees fahrenheit");
@@ -55,35 +68,41 @@ function runSearchfromSearch () {
         var windSpeed = $("<p>").text("Wind Speed: " + r.list[0].wind.speed + " MPH");
         currentConditionsEl.append(cityTitle.append(weatherIcon), cityTemp, cityHumidity, windSpeed);
         
+        // create & call second query for forecast & UVI data using previously gotten lat & lon
         fullQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=19ead611ac1f13b1c00469e35bb98b61";
         $.ajax({
             url: fullQueryURL,
             method: "GET"
         }).then(function(re) {
+            console.log(re);
+            // get UVindex, add general class
             var uvIndex = $("<p>").text("UV Index: " + re.current.uvi).attr("id", "uv");
+            // change new element color based on index #
             if (re.current.uvi < 3) {uvIndex.attr("id", "uv-low")};
             if (re.current.uvi > 8) {uvIndex.attr("id", "uv-high")};
             currentConditionsEl.append(uvIndex);
+            // get the forcast array from the ajax call
             var fiveDayForecast = re.daily
-            console.log(fiveDayForecast);
+            // loop through the array
             for (var i = 1; i < 6; i++) {
-                var fcard = $("<div>").attr("class", "col-sm-2")
-                var fdate = $("<h5>").text(moment().add(i,'days').format("L"));
+                // get date, weather icon, temp & humidity and add to div, append to page
+                var fcard = $("<div>").attr("class", "col-lg-2")
+                var fdate = $("<h6>").text(moment().add(i,'days').format("L"));
                 var ficon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + fiveDayForecast[i].weather[0].icon + "@2x.png");
                 var ftemp = $("<p>").text("Temp: " + ((fiveDayForecast[i].temp.day - 273.15)*1.8+32).toFixed(0) + "F");
                 var fhumidity = $("<p>").text("Humidity: " + fiveDayForecast[i].humidity + "%");
-
                 forecastEl.append(fcard.append(fdate, ficon, ftemp, fhumidity));
-
-
             }
             
         });
     });
+    // get item from local storage
     searchedArray = JSON.parse(localStorage.getItem("searched"));
+        // if no item, set to an empty array
         if (searchedArray === null) {
             searchedArray = [];
         };
+        // add new user input to 
         searchedArray.push(citySearch);
         localStorage.setItem("searched", JSON.stringify(searchedArray));
 };
@@ -117,8 +136,8 @@ function runSearchfromPrevious (cityName) {
             var fiveDayForecast = re.daily
             console.log(fiveDayForecast);
             for (var i = 1; i < 6; i++) {
-                var fcard = $("<div>").attr("class", "col-sm-2")
-                var fdate = $("<h5>").text(moment().add(i,'days').format("L"));
+                var fcard = $("<div>").attr("class", "col-lg-2")
+                var fdate = $("<h6>").text(moment().add(i,'days').format("L"));
                 var ficon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + fiveDayForecast[i].weather[0].icon + "@2x.png");
                 var ftemp = $("<p>").text("Temp: " + ((fiveDayForecast[i].temp.day - 273.15)*1.8+32).toFixed(0) + "F");
                 var fhumidity = $("<p>").text("Humidity: " + fiveDayForecast[i].humidity + "%");
@@ -130,6 +149,7 @@ function runSearchfromPrevious (cityName) {
             
         });
     });
+    
 }        
 
 // EVENT LISTENERS
@@ -138,10 +158,19 @@ searchButton.on("click", function () {
     loadSearched();
 });
 
-$(".psearch").on("click", function () {
+$(document).on("click", ".psearch", recall);
+
+cityInput.on('keypress', function (e) {
+    if (e.which === 13) {
+        runSearchfromSearch();
+        loadSearched();
+    };
+});
+
+function recall () {
     runSearchfromPrevious($(this).text());
     loadSearched();
-});
+}
 
 
 
